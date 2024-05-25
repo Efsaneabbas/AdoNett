383b368d3158296757a34210f467bd934b2abe83
Efsane <Efsane@DESKTOP-TMI95QM>
2024-05-26 01:38:23 +0400

    Add project files.

---
+++ AdoNett/Controller/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using Business.Services.Abstract;
+using Business.Services.Concretes;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNett.Controller
+{
+    public  class ProductController
+    {
+        IProductServices _productService = new ProductService();
+
+        public void AddProduct()
+        {
+            Console.WriteLine("Enter product Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter product price: ");
+            double price = double.Parse(Console.ReadLine());
+
+            Product product = new Product()
+            {
+                Name = name,
+                Price = price,
+            };
+
+            _productService.AddProduct(product);
+        }
+
+        public void DeleteProduct()
+        {
+            Console.WriteLine("Enter product id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            _productService.DeleteProduct(id);
+        }
+
+        public void UpdateProduct()
+        {
+            GetAllProducts();
+            Console.WriteLine("Enter product id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter product name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter product price: ");
+            double price = double.Parse(Console.ReadLine());
+
+            Product newProduct = new Product()
+            {
+                Name = name,
+                Price = price
+            };
+
+            _productService.UpdateProduct(id, newProduct);
+        }
+
+        public void GetAllProducts()
+        {
+            foreach (var item in _productService.GetAllProducts())
+            {
+                Console.WriteLine($"{item.Id} - {item.Name} - {item.Price}");
+            }
+        }
+
+        public void GetProduct()
+        {
+            Console.WriteLine("Enter product id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Product product = _productService.GetProduct(id);
+
+            Console.WriteLine($"{product.Id} - {product.Name} - {product.Price}");
+        }
+    }
+}
+
---
+++ AdoNett/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdoNett
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
